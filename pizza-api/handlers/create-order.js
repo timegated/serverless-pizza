fce8c2fd0f8fe6d1a30c0bb41cb05c34f04689c0
timegated <beccdaniel@gmail.com>
2022-07-19 21:36:47 -0700

    DynamoDB setup with Claudia api

--- pizza-api/handlers/create-order.js
+++ pizza-api/handlers/create-order.js
@@ -1,10 +1,28 @@
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
 
-const createOrder = (order) => {
-  if (!order || !order.pizzaId || !order.address) {
+const createOrder = (request) => {
+  if (!request || !request.pizza || !request.address) {
     throw new Error('To order pizza please provide a pizza type and adress')
   }
 
-  return {};
+  return docClient.put({
+    TableName: 'pizza-orders',
+    Item: {
+      orderId: 'some-id',
+      pizza: request.pizza,
+      address: request.address,
+      orderStatus: 'pending'
+    }
+  }).promise()
+    .then(res => {
+      console.log('Order successfully created');
+      return res;
+    })
+    .catch(e => {
+      console.error('Something went wrong with creating your order', e)
+      throw e;
+    });
 }
 
 module.exports = createOrder;
